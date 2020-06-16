473cb108e14682b3e60226c3b4e884a9ee71b929
Dipaditya Das <DipadityaDas@gmail.com>
2020-06-16 23:57:15 +0530

    Updated

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 print "Hello I am Diapaditya Das\n";
 print "This is a webpage which had been deployed Terraform on aws.\n";
 ?>
-<center><img src="profile.jpg" width="400px" height="400px"></center>
+<center><img src="Profile.jpg" width="400px" height="400px"></center>
 <pre>
